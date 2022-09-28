4940dc989d17d3d95f5c1e0cb135585723804860
Luca Santi <santilu@MacBook-Pro.station>
2022-09-28 09:10:50 +0200

    post/slug.js author and categories

---
+++ web/pages/post/[slug].js
@@ -0,0 +1,47 @@
+import groq from 'groq'
+import client from '../../client'
+
+const Post = ({post}) => {
+  const { title = 'Missing title', name = 'Missing name', categories } = post
+  return (
+    <article>
+      <h1>{title}</h1>
+      <span>By {name}</span>
+      {categories && (
+        <ul>
+          Posted in
+          {categories.map(category => <li key={category}>{category}</li>)}
+        </ul>
+      )}
+    </article>
+  )
+}
+
+const query = groq`*[_type == "post" && slug.current == $slug][0]{
+  title,
+  "name": author->name,
+  "categories": categories[]->title
+}`
+
+export async function getStaticPaths() {
+  const paths = await client.fetch(
+    groq`*[_type == "post" && defined(slug.current)][].slug.current`
+  )
+
+  return {
+    paths: paths.map((slug) => ({params: {slug}})),
+    fallback: true,
+  }
+}
+
+export async function getStaticProps(context) {
+  // It's important to default the slug so that it doesn't return "undefined"
+  const { slug = "" } = context.params
+  const post = await client.fetch(query, { slug })
+  return {
+    props: {
+      post
+    }
+  }
+}
+export default Post
